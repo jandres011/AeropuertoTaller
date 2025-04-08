c3403c89d585ad9dd0ceeba03277261e34de957e
xdoritoz <123668559+xdoritoz@users.noreply.github.com>
2025-04-08 18:59:56 -0500

    -Adding aereolineaService  -adding aereolineaServiceInterface

---
+++ src/main/java/edu/unimagdalena/aereopuerto/exepciones/ResourceNotFoundExeption.java
@@ -0,0 +1,9 @@
+package edu.unimagdalena.aereopuerto.exepciones;
+
+public class ResourceNotFoundExeption extends RuntimeException{
+    public ResourceNotFoundExeption(String message) {
+        super(message);
+    }
+
+
+}
